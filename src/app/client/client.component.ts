5af1e4af3b3be3f47d78761dee6badd58fcd4747
DefeNder93 <alexdefender93@gmail.com>
2017-09-29 00:47:56 +0800

    search

--- src/app/client/client.component.ts
+++ src/app/client/client.component.ts
@@ -11,16 +11,23 @@ export class ClientComponent implements OnInit {
   constructor(private api: Api) {}
 
   clients = [];
+  initialClients = [];
   currentPage = 1;
   itemsPerPage = 10;
+  searchQuery = '';
 
   ngOnInit() {
     this.getCients();
   }
 
+  search() {
+    this.clients = !this.searchQuery ? this.initialClients : this.initialClients.filter(c => c.ClientName.toLowerCase().indexOf(this.searchQuery.toLowerCase()) !== -1);
+  }
+
   public getCients() {
     this.api.getClients().subscribe(r => {
       this.clients = r.value;
+      this.initialClients = r.value;
     }, e => {
       console.log('e', e);
     });
